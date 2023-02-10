7246f532d23982f2c4a018b5f07a79f04cafc796
Omar Salem <omarsalem0721@gmail.com>
2023-02-10 17:05:49 +0200

    Configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,15 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
