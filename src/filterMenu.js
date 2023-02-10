25ae847ec48823e923059c88dfcf8d7524d893f8
Omar Salem <omarsalem0721@gmail.com>
2023-02-10 23:34:54 +0200

    Remove console and make the code clean

--- src/filterMenu.js
+++ src/filterMenu.js
@@ -1,7 +1,6 @@
 const filterMenuOptions = (selectTerm) => {
   const menuOptions = document.querySelectorAll('.menu-item');
-  [...menuOptions].forEach((item) => {
-    console.log(item.textContent.includes(selectTerm));
+  menuOptions.forEach((item) => {
     if (item.textContent.includes(selectTerm)) {
       item.style.display = 'block';
     } else {
