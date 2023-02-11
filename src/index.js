ecc554e1c3698ad4ff79ed854005657ec8dd808f
Omar Salem <omarsalem0721@gmail.com>
2023-02-11 15:41:10 +0200

    Add case when enter /1 the h1 applies

--- src/index.js
+++ src/index.js
@@ -5,35 +5,47 @@ import filterMenuOptions from './filterMenu.js';
 
 const contentContainer = document.getElementById('edit-container');
 const firstInput = document.getElementById('input-1');
+firstInput.focus();
 
 const newInputHandler = (editor = firstInput) => {
   editor.addEventListener('input', () => {
     if (editor.textContent[0] === '/') {
-      // create a selection menu
       createMenu(editor);
-      // select option
       selectOption(editor);
       filterMenuOptions(editor.textContent.slice(1));
     } else {
       removeMenu();
     }
   });
+  createNewLine(editor);
+};
 
+const createNewLine = (editor) => {
   // handle the case to create a new line
   editor.addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
       event.preventDefault();
-      if (editor.textContent === '') {
+      if (editor.textContent[0] === '/' && editor.textContent[1] === '1') {
+        // editor.className = 'input';
+        editor.classList.add('editable-input', 'heading1');
+        editor.setAttribute('data-placeholder', 'Heading 1');
+        editor.textContent = editor.textContent.slice(2);
+        editor.focus();
+        removeMenu();
+      } else if (editor.textContent === '') {
         editor.removeAttribute('data-placeholder');
+        const newEle = document.createElement('div');
+        newEle.classList.add('editable-input');
+        newEle.contentEditable = true;
+        newEle.setAttribute(
+          'data-placeholder',
+          'Type / for blocks, @ link docs'
+        );
+        newEle.id = 'input-1';
+        contentContainer.appendChild(newEle);
+        newEle.focus();
+        newInputHandler(newEle);
       }
-      const newEle = document.createElement('div');
-      newEle.classList.add('editable-input');
-      newEle.contentEditable = true;
-      newEle.setAttribute('data-placeholder', 'Type / for blocks, @ link docs');
-      newEle.id = 'input-1';
-      contentContainer.appendChild(newEle);
-      newEle.focus();
-      newInputHandler(newEle);
     }
   });
 };
